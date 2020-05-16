8f6ee70409431247a3b89cf68315d64aedd7b1e8
Wildlife <admin@lanseyujie.com>
2020-05-16 22:56:28 +0800

    update router helper

--- cardinal/router/helper.go
+++ cardinal/router/helper.go
@@ -1,7 +1,6 @@
 package router
 
 import (
-    "net/http"
     "strings"
 )
 
@@ -87,46 +86,4 @@ func (h *Helper) Any(handler HandlerFunc) *Helper {
     h.router.Any(h.group+h.target, handler)
 
     return h
-}
-
-func (r *Router) Head(pattern string, f HandlerFunc) {
-    if f != nil {
-        r.tree.Insert(http.MethodHead, pattern, f)
-    }
-}
-
-func (r *Router) Options(pattern string, f HandlerFunc) {
-    if f != nil {
-        r.tree.Insert(http.MethodOptions, pattern, f)
-    }
-}
-
-func (r *Router) Get(pattern string, f HandlerFunc) {
-    if f != nil {
-        r.tree.Insert(http.MethodGet, pattern, f)
-    }
-}
-
-func (r *Router) Post(pattern string, f HandlerFunc) {
-    if f != nil {
-        r.tree.Insert(http.MethodPost, pattern, f)
-    }
-}
-
-func (r *Router) Put(pattern string, f HandlerFunc) {
-    if f != nil {
-        r.tree.Insert(http.MethodPut, pattern, f)
-    }
-}
-
-func (r *Router) Delete(pattern string, f HandlerFunc) {
-    if f != nil {
-        r.tree.Insert(http.MethodDelete, pattern, f)
-    }
-}
-
-func (r *Router) Any(pattern string, f HandlerFunc) {
-    if f != nil {
-        r.tree.Insert("ANY", pattern, f)
-    }
 }
--- cardinal/router/router.go
+++ cardinal/router/router.go
@@ -1,6 +1,8 @@
 package router
 
-import "net/http"
+import (
+    "net/http"
+)
 
 // Router
 type Router struct {
@@ -24,6 +26,52 @@ func Default() *Router {
 // Static will quickly register a static file service route
 func Static(path string) {
     // TODO://
+}
+
+func (r *Router) Head(pattern string, f HandlerFunc) {
+    if f != nil {
+        r.tree.Insert(http.MethodHead, pattern, f)
+    }
+}
+
+func (r *Router) Options(pattern string, f HandlerFunc) {
+    if f != nil {
+        r.tree.Insert(http.MethodOptions, pattern, f)
+    }
+}
+
+func (r *Router) Get(pattern string, f HandlerFunc) {
+    if f != nil {
+        r.tree.Insert(http.MethodGet, pattern, f)
+    }
+}
+
+func (r *Router) Post(pattern string, f HandlerFunc) {
+    if f != nil {
+        r.tree.Insert(http.MethodPost, pattern, f)
+    }
+}
+
+func (r *Router) Put(pattern string, f HandlerFunc) {
+    if f != nil {
+        r.tree.Insert(http.MethodPut, pattern, f)
+    }
+}
+
+func (r *Router) Delete(pattern string, f HandlerFunc) {
+    if f != nil {
+        r.tree.Insert(http.MethodDelete, pattern, f)
+    }
+}
+
+func (r *Router) Any(pattern string, f HandlerFunc) {
+    if f != nil {
+        r.tree.Insert("ANY", pattern, f)
+    }
+}
+
+func (r *Router) Show() {
+    r.tree.Show(nil)
 }
 
 // ServeHTTP
