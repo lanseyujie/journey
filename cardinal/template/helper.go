11b93fa4d9a9d0550dd7b58fa93a0dd5e78251c2
Wildlife <admin@lanseyujie.com>
2020-05-24 16:10:38 +0800

    template func

---
+++ cardinal/template/helper.go
@@ -0,0 +1,37 @@
+package template
+
+import (
+    "html/template"
+    "time"
+)
+
+// Html
+func Html(str string) template.HTML {
+    return template.HTML(str)
+}
+
+// DateFormat
+func DateFormat(t time.Time, layout string) string {
+    return t.Format(layout)
+}
+
+// Substr
+func Substr(s string, start, length int) string {
+    chars := []rune(s)
+    if start < 0 {
+        start = 0
+    }
+
+    if start > len(chars) {
+        start = start % len(chars)
+    }
+
+    var end int
+    if (start + length) > (len(chars) - 1) {
+        end = len(chars)
+    } else {
+        end = start + length
+    }
+
+    return string(chars[start:end])
+}
