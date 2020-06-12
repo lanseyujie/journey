60f5157f3d7fa200e7e2bb905eb51820db8c3088
Wildlife <admin@lanseyujie.com>
2020-06-12 23:49:41 +0800

    implement sql query

--- cardinal/dao/dao.go
+++ cardinal/dao/dao.go
@@ -1,6 +1,12 @@
 package dao
 
+import (
+    "database/sql"
+    "strings"
+)
+
 type Dao struct {
+    isTx  bool
     table string
 }
 
@@ -12,14 +18,86 @@ func NewDao(table string) *Dao {
 
 // Exec
 func (dao *Dao) Exec(preSql string, params ...interface{}) (err error) {
-    // TODO://
+    var stmt *sql.Stmt
+    preSql = strings.TrimSpace(preSql)
+    if dao.isTx {
+        stmt, err = tx.Prepare(preSql)
+        if err != nil {
+            return
+        }
+    } else {
+        stmt, err = db.Prepare(preSql)
+        if err != nil {
+            return
+        }
+    }
+    defer stmt.Close()
+
+    _, err = stmt.Exec(params...)
 
     return
 }
 
 // Query
 func (dao *Dao) Query(preSql string, params ...interface{}) (result []map[string]interface{}, err error) {
-    // TODO://
+    var (
+        stmt *sql.Stmt
+        rows *sql.Rows
+        cols []string
+    )
+
+    preSql = strings.TrimSpace(preSql)
+    if dao.isTx {
+        stmt, err = tx.Prepare(preSql)
+        if err != nil {
+            return
+        }
+    } else {
+        stmt, err = db.Prepare(preSql)
+        if err != nil {
+            return
+        }
+    }
+    defer stmt.Close()
+
+    rows, err = stmt.Query(params...)
+    if err != nil {
+        return
+    }
+    defer rows.Close()
+
+    // get the column names of the query
+    cols, err = rows.Columns()
+    if err != nil {
+        return
+    }
+
+    count := len(cols)
+    // store the value of each field in a row
+    row := make([]sql.RawBytes, count)
+    scans := make([]interface{}, count)
+    // let each row of data be filled in []sql.RawBytes
+    for i := range row {
+        scans[i] = &row[i]
+    }
+
+    for rows.Next() {
+        // scans[i] = &values[i]
+        err = rows.Scan(scans...)
+        if err == nil {
+            // a row of data, column => value
+            values := make(map[string]interface{})
+            for i, v := range row {
+                if v != nil {
+                    values[cols[i]] = string(v)
+                } else {
+                    // SQL NULL
+                    values[cols[i]] = nil
+                }
+            }
+            result = append(result, values)
+        }
+    }
 
     return
 }
