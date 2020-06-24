4c495c7c1e5ed2b4c1c0041c0608027061c14614
Wildlife <admin@lanseyujie.com>
2020-06-24 23:18:20 +0800

    log dirver: console

---
+++ cardinal/log/console/console.go
@@ -0,0 +1,67 @@
+package console
+
+import (
+    "fmt"
+    "os"
+    "sync"
+)
+
+type Console struct {
+    lock *sync.Mutex
+}
+
+// var console = &Console{}
+
+func NewConsole() *Console {
+    return &Console{
+        lock: &sync.Mutex{},
+    }
+}
+
+// StringBlue
+func StringBlue(str string) string {
+    return fmt.Sprintf("%c[%d;%d;%dm%s%c[0m", 0x1B, 0, 94, 1, str, 0x1B)
+}
+
+// StringYellow
+func StringYellow(str string) string {
+    return fmt.Sprintf("%c[%d;%d;%dm%s%c[0m", 0x1B, 0, 93, 1, str, 0x1B)
+}
+
+// StringGreen
+func StringGreen(str string) string {
+    return fmt.Sprintf("%c[%d;%d;%dm%s%c[0m", 0x1B, 0, 92, 1, str, 0x1B)
+}
+
+// StringRed
+func StringRed(str string) string {
+    return fmt.Sprintf("%c[%d;%d;%dm%s%c[0m", 0x1B, 0, 91, 1, str, 0x1B)
+}
+
+// Write
+func (c *Console) Write(p []byte) (n int, err error) {
+    c.lock.Lock()
+    defer c.lock.Unlock()
+
+    n = len(p)
+    msg := string(p)
+    length := len("[INFO]")
+    if len(msg) >= length {
+        switch msg[:length] {
+        case "[INFO]":
+            _, err = fmt.Fprint(os.Stdout, StringBlue(msg))
+        case "[WARN]":
+            _, err = fmt.Fprint(os.Stderr, StringYellow(msg))
+        case "[ERRO]":
+            _, err = fmt.Fprint(os.Stderr, StringRed(msg))
+        case "[HTTP]":
+            _, err = fmt.Fprint(os.Stdout, StringGreen(msg))
+        default:
+            _, err = fmt.Fprint(os.Stdout, msg)
+        }
+    } else {
+        _, err = fmt.Fprint(os.Stdout, msg)
+    }
+
+    return
+}
