7ed4490d9a0ae9f3babaec216ddb56e4ce2aacae
Wildlife <admin@lanseyujie.com>
2020-07-01 00:28:25 +0800

    cache driver register

--- cardinal/cache/cache.go
+++ cardinal/cache/cache.go
@@ -3,11 +3,32 @@ package cache
 import "time"
 
 type Cache interface {
+    Init() error
     Exist(key string) bool
     Get(key string) interface{}
-    Put(key string, value interface{}, timeout time.Duration) error
+    Put(key string, value interface{}, lifetime time.Duration) error
     Del(key string) error
     Incr(key string) error
     Decr(key string) error
     Drop() error
+}
+
+var adapters = make(map[string]Cache)
+
+// Register
+func Register(name string, cache Cache) {
+    if cache == nil {
+        panic("cache: adapter is nil")
+    }
+    if _, ok := adapters[name]; ok {
+        panic("cache: register adapter repeatedly")
+    }
+    adapters[name] = cache
+}
+
+// Get
+func Get(name string) (cache Cache, exist bool) {
+    cache, exist = adapters[name]
+
+    return
 }
