22c116e6dad0a937a01fe4160670c48a9efffe52
Wildlife <admin@lanseyujie.com>
2020-09-04 23:40:47 +0800

    remove unused files

--- journey.go
+++ journey.go
@@ -1,7 +1,28 @@
-package main
+package journey
 
 import "fmt"
 
-func main() {
-    fmt.Println("Hello World")
+const (
+	banner = `
+       __                                 
+      / /___  __  ___________  ___  __  __
+ __  / / __ \/ / / / ___/ __ \/ _ \/ / / /
+/ /_/ / /_/ / /_/ / /  / / / /  __/ /_/ / 
+\____/\____/\__,_/_/  /_/ /_/\___/\__, /  
+                                 /____/    v%s (%s)
+
+high performance, minimalist blog framework
+https://lanseyujie.com
+
+`
+)
+
+var (
+	Version    = "1.0.0"
+	LastCommit = "0000000"
+	BuildDate  = ""
+)
+
+func init() {
+	fmt.Printf(banner, Version, LastCommit)
 }
